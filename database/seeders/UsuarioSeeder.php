f21b2753068140d5859c233cf331528699c1e330
Edgar Mamani Gutierrez <edgel.infor@gmail.com>
2020-09-19 23:59:44 -0400

    BD recetas y Seeder Categorias - Usuarios

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Edgar',
+            'email' => 'edgar@correo.com',
+            'password' => Hash::make('12345678'),
+            'url' => 'http://devedgelweb.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Marcos',
+            'email' => 'marcos@correo.com',
+            'password' => Hash::make('12345678'),
+            'url' => 'http://marcosweb.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
